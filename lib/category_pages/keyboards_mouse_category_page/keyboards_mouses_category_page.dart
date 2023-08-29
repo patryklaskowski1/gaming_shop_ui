831dca1e97dfa486522e8b06a4466127b1eba8d5
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-08-29 16:04:42 +0200

    Create keyboards and mouses category page

---
+++ lib/category_pages/keyboards_mouse_category_page/keyboards_mouses_category_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:gaming_shop_ui/guides/guides_view.dart';
+import 'package:gaming_shop_ui/home_page/widgets/app_bar.dart';
+import 'package:gaming_shop_ui/home_page/widgets/back_drawer_menu.dart';
+import 'package:gaming_shop_ui/home_page/widgets/bottom_navigator_bar.dart';
+import 'package:gaming_shop_ui/itemsModel/keyboards_view_model.dart';
+import 'package:gaming_shop_ui/itemsModel/laptop_view_model.dart';
+import 'package:gaming_shop_ui/itemsModel/mouse_view_model.dart';
+import 'package:gaming_shop_ui/itemsModel/pcs_view_model.dart';
+
+class KeyboardsMouseCategoryPage extends StatefulWidget {
+  const KeyboardsMouseCategoryPage({
+    super.key,
+  });
+
+  @override
+  State<KeyboardsMouseCategoryPage> createState() =>
+      _KeyboardsMouseCategoryPageState();
+}
+
+class _KeyboardsMouseCategoryPageState
+    extends State<KeyboardsMouseCategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const BackDrawerMenu(),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBarView(),
+      ),
+      body: Column(
+        children: [
+          const GuidesView(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: GridView.count(
+                childAspectRatio: MediaQuery.of(context).size.width /
+                    (MediaQuery.of(context).size.height),
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 10,
+                children: const [
+                  KeyboardsViewModel(),
+                  MouseViewModel(),
+                  KeyboardsViewModel(),
+                  MouseViewModel(),
+                  KeyboardsViewModel(),
+                  MouseViewModel(),
+                  KeyboardsViewModel(),
+                  MouseViewModel(),
+                  KeyboardsViewModel(),
+                  MouseViewModel(),
+                  KeyboardsViewModel(),
+                  MouseViewModel(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: const BottomNavigatorBarDesign(),
+    );
+  }
+}
--- lib/home_page/widgets/back_drawer_menu.dart
+++ lib/home_page/widgets/back_drawer_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gaming_shop_ui/category_pages/keyboards_mouse_category_page/keyboards_mouses_category_page.dart';
 import 'package:gaming_shop_ui/category_pages/monitors_category_page/monitors_category_page.dart';
 import 'package:gaming_shop_ui/category_pages/pc_laptop_category_page/pcs_laptops_category_page.dart';
 import 'package:gaming_shop_ui/const/const.dart';
@@ -72,7 +73,12 @@ class BackDrawerMenu extends StatelessWidget {
               ),
             ),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const KeyboardsMouseCategoryPage(),
+                ),
+              );
             },
           ),
           ListTile(

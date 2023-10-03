7ef342cfb367807339a0e2e3054b026603ff5003
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-10-03 16:31:38 +0200

    install firebase auth

---
+++ lib/RootPage/root_page.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Jesteś niezalogowany'),
+              ),
+            );
+          }
+          return const Scaffold();
+        });
+  }
+}
